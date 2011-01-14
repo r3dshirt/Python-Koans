b01e06fc97b03d4f8c42b161fe52989bd550bca8
Sam Knowles <sam.knowles@gmail.com>
2011-01-14 16:38:24 +1300

    completed about iteration

--- python 3/koans/about_iteration.py
+++ python 3/koans/about_iteration.py
@@ -7,26 +7,26 @@ class AboutIteration(Koan):
 
     def test_iterators_are_a_type(self):
         it = iter(range(1,6))
-        
+
         fib = 0
-        
+
         for num in it:
             fib += num
-            
-        self.assertEqual(__ , fib)
+
+        self.assertEqual(15 , fib)
 
     def test_iterating_with_next(self):
         stages = iter(['alpha','beta','gamma'])
 
         try:
-            self.assertEqual(__, next(stages))
+            self.assertEqual('alpha', next(stages))
             next(stages)
-            self.assertEqual(__, next(stages))
+            self.assertEqual('gamma', next(stages))
             next(stages)
         except StopIteration as ex:
             err_msg = 'Ran out of iterations'
-            
-        self.assertRegexpMatches(err_msg, __)         
+
+        self.assertRegexpMatches(err_msg, 'Ran out of iterations')
 
     # ------------------------------------------------------------------
 
@@ -36,36 +36,36 @@ class AboutIteration(Koan):
     def test_map_transforms_elements_of_a_list(self):
         seq = [1, 2, 3]
         mapped_seq = list()
-   
+
         mapping = map(self.add_ten, seq)
-        
+
         self.assertNotEqual(list, type(mapping).__name__)
-        self.assertEqual(__, type(mapping).__name__)
+        self.assertEqual('map', type(mapping).__name__)
         # In Python 3 built in iterator funcs return iteratable view objects
         # instead of lists
-   
+
         for item in mapping:
             mapped_seq.append(item)
-            
-        self.assertEqual(__, mapped_seq)
-        
+
+        self.assertEqual([11, 12, 13], mapped_seq)
+
         # None, iterator methods actually return objects of iter type in
         # python 3. In python 2 map() would give you a list.
-        
+
     def test_filter_selects_certain_items_from_a_list(self):
         def is_even(item): return (item % 2) == 0
 
         seq = [1, 2, 3, 4, 5, 6]
         even_numbers = list()
-   
+
         for item in filter(is_even, seq):
             even_numbers.append(item)
-            
-        self.assertEqual(__, even_numbers)
-    
+
+        self.assertEqual([2, 4, 6], even_numbers)
+
     def test_just_return_first_item_found(self):
         def is_big_name(item): return len(item) > 4
-        
+
         names = ["Jim", "Bill", "Clarence", "Doug", "Eli"]
         name = None
 
@@ -75,8 +75,8 @@ class AboutIteration(Koan):
         except StopIteration:
             msg = 'Ran out of big names'
 
-        self.assertEqual(__, name)
-        
+        self.assertEqual('Clarence', name)
+
 
     # ------------------------------------------------------------------
 
@@ -85,55 +85,55 @@ class AboutIteration(Koan):
 
     def multiply(self,accum,item):
         return accum * item
-        
+
     def test_reduce_will_blow_your_mind(self):
         import functools
         # As of Python 3 reduce() has been demoted from a builtin function
         # to the functools module.
-        
+
         result = functools.reduce(self.add, [2, 3, 4])
-        self.assertEqual(__, type(result).__name__)
+        self.assertEqual('int', type(result).__name__)
         # Reduce() syntax is same as Python 2
-        
-        self.assertEqual(__, result)
-    
-        result2 = functools.reduce(self.multiply, [2, 3, 4], 1) 
-        self.assertEqual(__, result2)
-    
+
+        self.assertEqual(9, result)
+
+        result2 = functools.reduce(self.multiply, [2, 3, 4], 1)
+        self.assertEqual(24, result2)
+
         # Extra Credit:
         # Describe in your own words what reduce does.
-    
+
     # ------------------------------------------------------------------
 
-    def test_creating_lists_with_list_comprehensions(self):        
+    def test_creating_lists_with_list_comprehensions(self):
         feast = ['lambs', 'sloths', 'orangutans', 'breakfast cereals', 'fruit bats']
-        
+
         comprehension = [delicacy.capitalize() for delicacy in feast]
-        
-        self.assertEqual(__, comprehension[0])
-        self.assertEqual(__, comprehension[2])
-        
+
+        self.assertEqual('Lambs', comprehension[0])
+        self.assertEqual('Orangutans', comprehension[2])
+
     def test_use_pass_for_iterations_with_no_body(self):
         for num in range(1,5):
             pass
-                
-        self.assertEqual(__, num)
+
+        self.assertEqual(4, num)
 
     # ------------------------------------------------------------------
-        
+
     def test_all_iteration_methods_work_on_any_sequence_not_just_lists(self):
         # Ranges are an iteratable sequence
         result = map(self.add_ten, range(1,4))
-        self.assertEqual(__, list(result))
+        self.assertEqual([11, 12, 13], list(result))
 
         try:
             # Files act like a collection of lines
             file = open("example_file.txt")
-    
+
             def make_upcase(line) : return line.strip().upper()
             upcase_lines = map(make_upcase, file.readlines())
-            self.assertEqual(__, list(upcase_lines))
-            
+            self.assertEqual(['THIS', 'IS', 'A', 'TEST'], list(upcase_lines))
+
             # NOTE: You can create your own collections that work with each,
             # map, select, etc.
         finally:
